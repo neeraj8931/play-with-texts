c8384e7e826465348750f5fe0bc6add2261a1c9e
Neeraj Shukla <er.neerajshukla.1010@gmail.com>
2021-10-05 22:21:12 +0530

    PlayWithText App v1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Textbox from './component/Textbox';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+    <div className="row"> 
+            <Textbox className="m-3"></Textbox>
     </div>
+    <div class="fixed-bottom">
+    <div class="footer bg-green">
+      <div className="text-center text-white">
+          <p>Made with ❤ by <a href="#"><strong>Neeraj </strong></a></p>
+          <div className="social-icons text-white">
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-github"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-linkedin-in"></i></a>
+          </div>
+      </div>
+    </div>
+    </div>
+    
+   </>
   );
 }
 
